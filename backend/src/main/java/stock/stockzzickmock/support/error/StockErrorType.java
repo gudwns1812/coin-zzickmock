df843f0a95d74443a21c99a84570c3a59845baa1
gudwns1812 <gudwns1812@naver.com>
2026-04-08 11:33:45 +0900

    feat: [backend] 활성 종목 집합 Redis Stream 발행 기능 추가

--- backend/src/main/java/stock/stockzzickmock/support/error/StockErrorType.java
+++ backend/src/main/java/stock/stockzzickmock/support/error/StockErrorType.java
@@ -8,6 +8,10 @@ public enum StockErrorType implements ErrorType {
     INDICES_NOT_FOUND("STOCK_404_INDICES_NOT_FOUND", "지수 관련 정보를 찾지 못했습니다.", LogLevel.WARN, HttpStatus.NOT_FOUND),
     POPULAR_NOT_FOUND("STOCK_404_POPULAR_NOT_FOUND", "인기종목을 찾지 못했습니다.", LogLevel.WARN, HttpStatus.NOT_FOUND),
     STOCK_NOT_FOUND("STOCK_404_STOCK_NOT_FOUND", "주식을 찾지 못했습니다.", LogLevel.WARN, HttpStatus.NOT_FOUND),
+    STOCK_ACTIVE_SET_PUBLISH_FAILED("STOCK_500_ACTIVE_SET_PUBLISH_FAILED", "활성 종목 집합을 적재하지 못했습니다.", LogLevel.ERROR,
+            HttpStatus.INTERNAL_SERVER_ERROR),
+    STOCK_REQUEST_PUBLISH_FAILED("STOCK_500_REQUEST_PUBLISH_FAILED", "종목 수집 요청을 적재하지 못했습니다.", LogLevel.ERROR,
+            HttpStatus.INTERNAL_SERVER_ERROR),
     EMPTY_SEARCH_KEYWORD("STOCK_200_EMPTY_SEARCH_KEYWORD", "검색어가 비어있습니다.", LogLevel.INFO, HttpStatus.OK);
 
     private final String errorCode;
